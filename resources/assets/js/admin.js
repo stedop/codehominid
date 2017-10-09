5e8aacc1fe4e675825199b996d17e98c23a61fab
SteDop <dopstephen@gmail.com>
2017-10-09 15:03:55 +0100

    added my app to the admin view

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -21,12 +20,5 @@ Vue.use(Vuetify);
  */
 
 const app = new Vue({
-    el: '#app',
-
-    data: () => ({
-        drawer: true
-    }),
-    props: {
-        source: String
-    }
+    el: '#admin'
 });
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -8,10 +8,59 @@
 
     <title>Monkey around</title>
 
-    <link rel="stylesheet" href=""{{ mix('/css/app.css') }}">
+    <link rel="stylesheet" href=""{{ mix('/css/admin.css') }}">
 </head>
 <body id="app">
+    <v-app id="inspire" dark>
+        <v-navigation-drawer
+                clipped
+                persistent
+                v-model="drawer"
+                enable-resize-watcher
+                app
+        >
+            <v-list dense>
+                <v-list-tile @click="">
+                    <v-list-tile-action>
+                        <v-icon>dashboard</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Dashboard</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile @click="">
+                    <v-list-tile-action>
+                        <v-icon>settings</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Settings</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar app fixed clipped-left>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>Application</v-toolbar-title>
+        </v-toolbar>
+        <main>
+            <v-content>
+                <v-container fluid fill-height>
+                    <v-layout justify-center align-center>
+                        <v-tooltip right>
+                            <v-btn icon large :href="source" target="_blank" slot="activator">
+                                <v-icon large>code</v-icon>
+                            </v-btn>
+                            <span>Source</span>
+                        </v-tooltip>
+                    </v-layout>
+                </v-container>
+            </v-content>
+        </main>
+        <v-footer app fixed>
+            <span>&copy; 2017</span>
+        </v-footer>
+    </v-app>
 
-<script src="{{ mix('js/app.js') }}"></script>
+    <script src="{{ mix('js/admin.js') }}"></script>
 </body>
 </html>
