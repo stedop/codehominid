a3e014e9f61bb6558104d1025fe31debfab9c982
ste <dopstephne@gmail.com>
2017-10-10 02:15:08 +0100

    first fit of the post model and controller

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -12,28 +12,24 @@ class CategoryTableSeeder extends Seeder {
 		// Test category - 1
 		Category::create(array(
 				'name' => 'Test category - 1',
-				'slug' => 'test-category-1',
 				'description' => 'Test category - 1 Meta Desc'
 			));
 
 		// Test category - 2
 		Category::create(array(
 				'name' => 'Test category - 2',
-				'slug' => 'test-category-2',
 				'description' => 'Test category - 2 Meta Desc'
 			));
 
 		// Test category - 3
 		Category::create(array(
 				'name' => 'Test category - 3',
-				'slug' => 'test-category-3',
 				'description' => 'Test category - 3 Meta Desc'
 			));
 
 		// Test category - 4
 		Category::create(array(
 				'name' => 'Test category - 4',
-				'slug' => 'test-category-4',
 				'description' => 'Test category - 4 Meta Desc'
 			));
 	}
